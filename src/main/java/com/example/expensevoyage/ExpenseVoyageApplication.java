b4590f6a8bbd3ec5e6138abd8da60974950f1b16
Franklin <writefranklyn@gmail.com>
2024-09-18 23:59:12 +0100

    set up files

---
+++ src/main/java/com/example/expensevoyage/ExpenseVoyageApplication.java
@@ -0,0 +1,13 @@
+package com.example.expensevoyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseVoyageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseVoyageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/expensevoyage/ExpenseVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expensevoyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseVoyageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
